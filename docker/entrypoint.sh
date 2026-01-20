79ab31222769c90be72cde242bf864f1f90d2004
mosa0519 <mohanakrishna.sadhu@netcracker.com>
2026-01-20 15:37:21 +0530

    fix: support for RO filesystem

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+WRITABLE_CACERTS="/java-security/cacerts"
+ORIGINAL_CACERTS="${JAVA_HOME}/lib/security/cacerts"
+mkdir -p /java-security
+cp "${ORIGINAL_CACERTS}" "${WRITABLE_CACERTS}"
+export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS} -Djavax.net.ssl.trustStore=${WRITABLE_CACERTS} -Djavax.net.ssl.trustStorePassword=changeit"
+
 if [[ "$(ls ${TRUST_CERTS_DIR})" ]]; then
     for filename in ${TRUST_CERTS_DIR}/*; do
         echo "Import $filename certificate to Java cacerts"
-        ${JAVA_HOME}/bin/keytool -import -trustcacerts -keystore ${JAVA_HOME}/lib/security/cacerts -storepass changeit -noprompt -alias ${filename} -file ${filename}
+        ${JAVA_HOME}/bin/keytool -import -trustcacerts -keystore "${WRITABLE_CACERTS}" -storepass changeit -noprompt -alias "$(basename ${filename})" -file "${filename}"
     done;
 fi
 
